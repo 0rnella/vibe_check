11d3c01f61a31c97ddc04c44e0148d32b37161ad
ofriggit <ornella.friggit@disneystreaming.com>
2020-09-06 20:58:48 -0400

    Refactor server to add DB sync function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./database');
 
 const app = express();
 const port = 8000;
@@ -7,6 +8,21 @@ app.get('/', (req, res) => {
 	res.send('VIBE CHECK!');
 });
 
-app.listen(port, () => {
-	console.log(`Vibe check at http://localhost:${port}`);
-});
+// Make function to sync database
+const syncDb = () => db.sync({ force: false });
+
+// Make function for the node server to start (listen)
+const startListen = () => {
+	app.listen(port, () => {
+		console.log(`Vibe check at http://localhost:${port}`);
+	});
+};
+
+// Make function that calls all the necessary setup functions
+async function bootApp() {
+	await syncDb();
+	await startListen();
+}
+
+// Call this function to start all the setup functions
+bootApp();
