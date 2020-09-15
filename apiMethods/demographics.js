67956a3c0ecf2ff8665105ff8527d27b6df6504d
pyuli15 <pyuli15@hotmail.com>
2020-09-15 19:40:02 -0400

    Successfully fetched demographics

--- apiMethods/demographics.js
+++ apiMethods/demographics.js
@@ -1,5 +1,11 @@
-const getCompanyDemographics = (id) => {
+const base = require('./base');
+
+const getCompanyDemographics = async (id) => {
 	// now get the actual data in here
+	const { fields } = await base('Demographics').find(id);
+	delete fields.name;
+	delete fields.company;
+	return fields;
 };
 
 module.exports = {
